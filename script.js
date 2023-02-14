e03b0d677d9d5dd8a56f72b89dd0015a40c9ee49
krishna7462 <117273752+krishna7462@users.noreply.github.com>
2023-02-14 21:58:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your code here
+function mincost(event){
+	event.preventDefault();
+	var input = document.querySelector('input').value;
+	var arr=input.split(",");
+	arr.sort( function(a,b){return a-b});
+	var cost =0;
+	while(arr.length>1){
+        var res = arr[0]+arr[1];
+		cost +=res;
+		arr.push(res);
+		arr.shift();
+		arr.shift();
+		arr.sort(function(a,b){return a-b});
+	}
+	doument.qureySelector('#result').innerText = cost;
+}
+
+
+
+
