cde24dbd11904bd8d2688f7091e9231fea239432
krishna7462 <117273752+krishna7462@users.noreply.github.com>
2023-03-03 12:47:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ function mincost(event){
 	arr.sort( function(a,b){return a-b});
 	var cost =0;
 	while(arr.length>1){
-        var res = arr[0]+arr[1];
+        var res = parseInt(arr[0])+parseInt(arr[1]);
 		cost +=res;
 		arr.push(res);
 		arr.shift();
 		arr.shift();
 		arr.sort(function(a,b){return a-b});
 	}
-	doument.qureySelector('#result').innerText = cost;
+	document.getElementById('result').innerText = cost;
 }
 
 
